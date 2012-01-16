48a7ce3584fd57be092c8c5df890af1f0e4ae1a0
Derek Eder <derek.eder@gmail.com>
2012-01-15 18:16:46 -0600

    ward year placeholders and detail panels

--- lib/chicago/crime/view_helpers.rb
+++ lib/chicago/crime/view_helpers.rb
@@ -8,6 +8,25 @@ module Chicago
       def current_menu_class(menu_name)
         return "current" if current_menu == menu_name
       end
+
+      def ward_image()
+        return <<-src_tag
+http://maps.googleapis.com/maps/api/staticmap?
+size=138x100
+&sensor=false
+&path=
+color:0xc10202aa
+|fillcolor:0xc1020211
+|weight:1
+|enc:ecp~FfwyuOs@fiBpx@iCd^vI|NmErP{K~FsVvGg`@rHaOnlAeAr@rVpHdB?~Lpp@ePX~MjiAeBxGoFpd@kDX
+&maptype=roadmap
+&style=feature:road|visibility:off|saturation:-100
+&style=feature:landscape|saturation:-100|lightness:75
+&style=feature:transit|visibility:off
+&style=feature:poi|saturation:-100|lightness:60
+&style=feature:water|hue:0x00b2ff
+        src_tag
+      end
     end
   end
 end
