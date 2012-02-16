490ff0154063c1eb4af1a0f77c10806d48cf01f3
Derek Eder <derek.eder@gmail.com>
2012-02-16 00:23:45 -0600

    using fusion tables to render ward boundaries

---
+++ public/javascripts/fusiontables.ward.js
@@ -0,0 +1,99 @@
+var FusiontableWard = {};
+var map = null;
+var fusionTableId = 2954091;
+var map_bounds = new google.maps.LatLngBounds();
+FusiontableWard.create = function(number) {
+  var chicago = new google.maps.LatLng(41.8781136, -87.66677856445312);
+  
+  var simpleWardStyles = [
+    {
+      featureType: "road",
+      stylers: [
+        { visibility: "off" },
+        { saturation: -100 }
+      ]
+    },
+    {
+      featureType: "landscape",
+      stylers: [
+        { lightness: 75 },
+        { saturation: -100 }
+      ]
+    },
+    {
+      featureType: "transit",
+      stylers: [
+        { visibility: "off" }
+      ]
+    },
+    {
+      featureType: "poi",
+      stylers: [
+        { lightness: 60 },
+        { saturation: -100 }
+      ]
+    },
+    {
+      featureType: "water",
+      stylers: [
+        { hue: "#00b2ff" }
+      ]
+    }
+  ];
+
+  var myOptions = {
+    zoom: 9,
+    center: chicago,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    scrollwheel: false,
+    draggable: false,
+    streetViewControl: false,
+    zoomControl: false,
+    suppressInfoWindows: true
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas_detail"), myOptions);
+  map.setOptions({styles: simpleWardStyles});
+  
+  var wardKML = new google.maps.FusionTablesLayer(fusionTableId, {
+    query: "SELECT geometry FROM " + fusionTableId + " WHERE name = " + number,
+    suppressInfoWindows: true
+    }
+  );
+  
+  wardKML.setMap(map);
+  FusiontableWard.getMapBounds(number);
+}
+
+FusiontableWard.getMapBounds = function(number) {
+	//set the query using the parameter
+  var searchStr = "SELECT geometry FROM " + fusionTableId + " WHERE name = '" + number + "'";
+  
+  //set the callback function
+  FusiontableWard.getFTQuery(searchStr).send(FusiontableWard.setMapBounds);
+
+}
+
+FusiontableWard.setMapBounds = function(response) {
+  if (response.getDataTable().getNumberOfRows() > 0)
+  {
+    var kml = response.getDataTable().getValue(0, 0);
+    kml = kml.replace("<Polygon><outerBoundaryIs><LinearRing><coordinates>", "");
+    kml = kml.replace("</coordinates></LinearRing></outerBoundaryIs></Polygon>", "");
+    var boundPoints = kml.split(" ");
+    for(var i=0; i<boundPoints.length; i++) { 
+      var boundItem = boundPoints[i].split(",");
+      //console.log(boundItem[0]);
+      //console.log(boundItem[1]);
+      var point = new google.maps.LatLng(parseFloat(boundItem[1]), parseFloat(boundItem[0]));
+      
+      map_bounds.extend(point);
+    }
+    map.fitBounds(map_bounds);
+  }
+}
+
+FusiontableWard.getFTQuery = function(sql) {
+		var queryText = encodeURIComponent(sql);
+		return new google.visualization.Query('http://www.google.com/fusiontables/gvizdata?tq='  + queryText);
+	}
