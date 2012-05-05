2e14d5ceb44d90acda8c35970fdfbf9c3f4901dd
Chad W Pry <chad.pry@gmail.com>
2012-05-05 15:59:54 -0500

    remove old code

--- lib/crime/cacheable.rb
+++ lib/crime/cacheable.rb
@@ -16,22 +16,11 @@ module Cacheable
   def retain_in_cache(key)
     if true# settings.cacheable?
       sha_key = sha(key.to_s)
-#      @@dalli ||= Dalli::Client.new(settings.cacheable[:servers])
-#      if development?
-#        @@dalli ||= Dalli::Client.new("localhost:11211")
-#      elsif production?
-#        @@dalli ||= Dalli::Client.new("#{ENV["MEMCACHE_SERVERS"]}:11211", {
-#          :username => "#{ENV["MEMCACHE_USERNAME"]}",
-#          :password => "#{ENV["MEMCACHE_PASSWORD"]}"
-#        })
-#      end
 
-      #poky_operation = @@dalli.get(sha_key)
       poky_operation = settings.cache.get(sha_key)
       unless poky_operation
         poky_operation = yield
         settings.cache.set(sha_key, poky_operation)
-        #@@dalli.set(sha_key, poky_operation)
       end
       poky_operation
     else
